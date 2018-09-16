c625fefa86833345d97268b2f28a64db81a6c26e
shoki-0303 <shoki522.33333@gmail.com>
2018-09-16 21:06:58 +0900

    create Main and Sub content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import MainContent from './MainContent'
+import SubContent from './SubContent'
 
 class Content extends React.Component {
   render() {
     return (
-      <div>content</div>
-    );    
+      <React.Fragment>
+        <MainContent/>
+        <SubContent/>
+      </React.Fragment>
+    );
   }
 }
 
---
+++ src/components/MainContent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class MainContent extends React.Component {
+  render() {
+    return (
+      <div>Main Content</div>
+    );
+  }
+}
+
+export default MainContent
