05129e8c498a97cdc4b5e0aa4b039760d046206b
shoki-0303 <shoki522.33333@gmail.com>
2018-09-16 18:43:24 +0900

    import reset.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
+import './reset.css'
 
 import Container from './components/Container'
 
