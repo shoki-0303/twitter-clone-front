beb47041be3db935fb9284a8450fe52b87ff462c
shoki-0303 <shoki522.33333@gmail.com>
2018-09-16 18:29:08 +0900

    add styledComponent

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const View = styled.h2`
+  margin-left: 20px;
+`
 
 class Container extends React.Component {
   render() {
     return(
-      <h2>container</h2>
+      <View>container</View>
     );
   }
 }
