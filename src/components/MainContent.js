c7416f08864d545a5f9565b696f27083b5b6f09c
shoki-0303 <shoki522.33333@gmail.com>
2018-09-16 21:34:36 +0900

    add axis

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import axios from 'axios'
 
 class MainContent extends React.Component {
+  constructor(props) {
+    super(props)
+    axios.get('http://localhost:3001/api/v1/tweets')
+    .then((response)=>{
+      console.log(response)
+    })
+    .error((error)=>{
+      console.log(error)
+    })
+  }
   render() {
     return (
       <div>Main Content</div>
