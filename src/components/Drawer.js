50d863af6bc9f0549a5a426b9dade812f606f9f5
shoki-0303 <shoki522.33333@gmail.com>
2018-09-18 14:19:28 +0900

    create openDrawer function

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,26 +1,39 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const DRAWER_WIDTH = 240
+import styled, { css } from 'styled-components'
 
 const Wrapper = styled.div`
   position: fixed;
   top: 0;
-  left: -${DRAWER_WIDTH}px;
-  width: ${DRAWER_WIDTH}px;
+  left: -150px;
+  width: 150px;
   height: 100%;
   overflow-x: hidden;
   overflow-y: scroll;
   z-index: 10000;
   background-color: #f2f2f2;
+
+  -webkit-transition: all 0.15s ease-in;
+  -moz-transition: all 0.15s ease-in;
+  -o-transition: all 0.15s ease-in;
+  transition: all 0.15s ease-in;
+
+  ${(props) => 
+    props.isOpenDrawer &&
+    css`
+      -webkit-transform: translate3d(150px, 0, 0);
+      -moz-transform: translate3d(150px, 0, 0);
+      transform: translate3d(150px, 0, 0);
+      box-shadow: rgba(0, 0, 0, 0.12) -2px 0px 4px;    
+    `
+  }
 `
 
 class Drawer extends React.Component {
   render() {
     const {isOpenDrawer} = this.props.common
-    console.log(isOpenDrawer)
     return (
-      <Wrapper></Wrapper>
+      <Wrapper isOpenDrawer={isOpenDrawer}>
+      </Wrapper>
     );
   }
 }
