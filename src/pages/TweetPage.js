8bb510732ff474b47d99fd3f1a4b65fb88a1a1d5
shoki-0303 <shoki522.33333@gmail.com>
2018-09-21 14:34:33 +0900

    set view

--- src/pages/TweetPage.js
+++ src/pages/TweetPage.js
@@ -1,4 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Cell = styled.div`
+  padding: 0 15px;
+`
+const Category = styled.p`
+  display: inline-block;
+  color: #fff;
+  background-color: #4fb94f78;
+  margin-top: 15px;
+  margin-left: 10px;
+  padding: 8px 18px;
+`
+const Title = styled.h2`
+  font-size: 20px;
+  padding-top: 20px;
+  padding-left: 10px;
+`
+const Image = styled.div`
+  margin: 20px auto 0;
+  height: 240px;
+  width: 360px;
+  background-color: #80808047;
+  background-size: cover;
+  background-image: ${({imageUrl})=>`url(${imageUrl})`};
+`
+const Description = styled.p`
+  display: block;
+  padding-top: 25px;
+  line-height: 25px;
+`
 
 class TweetPage extends React.Component {
   constructor(props) {
@@ -7,8 +38,15 @@ class TweetPage extends React.Component {
     this.props.tweetsAction.fetchTweet(id)
   }
   render() {
+    console.log(this.props)
+    const { title, image, body } = this.props.tweet
     return (
-      <h2>TweetPage</h2>
+      <Cell>
+        <Title>{title}</Title>
+        <Category>fashion</Category>
+        <Image imageUrl={image}/>
+        <Description>{body}</Description>
+      </Cell>
     );
   }
 }
