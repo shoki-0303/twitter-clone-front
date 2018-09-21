6ce906e51e21428a12ba372ca010b0a2712cc229
shoki-0303 <shoki522.33333@gmail.com>
2018-09-21 13:15:35 +0900

    create TweetPage

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TweetsPage from '../Conteiners/TweetsPage'
+import TweetPage from '../pages/TweetPage'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 class MainContent extends React.Component {
---
+++ src/pages/TweetPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class TweetPage extends React.Component {
+  render() {
+    return (
+      <h2>TweetPage</h2>
+    );
+  }
+}
+
+export default TweetPage
