c79626d192b34f24dadefded3b9572a66ae1e959
shoki-0303 <shoki522.33333@gmail.com>
2018-09-23 19:30:30 +0900

    include user in combineReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
 import { tweets } from './tweets'
 import { common } from './common'
+import { user } from './user'
 
-export default combineReducers({tweets, common})
+export default combineReducers({tweets, common, user})
