199f08771ddcd5fd91284aa88112939bac07463d
shoki-0303 <shoki522.33333@gmail.com>
2018-09-24 22:29:22 +0900

    create 404ErrorPage

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Wrapper = styled.div`
   padding: 0 15px;
@@ -16,8 +17,17 @@ const Message = styled.p`
 `
 const Content = styled.p`
   padding-top: 10px;
+  padding-bottom: 20px;
   font-size: 13px;
   line-height: 20px;
+  text-align: center;
+`
+const Btn = styled.div`
+  color: white;
+  background-color: pink;
+  display: inline-block;
+  padding: 10px 15px;
+  margin: 0 auto;
 `
 
 const Error = ({statusCode}) => {
@@ -36,6 +46,9 @@ const Error = ({statusCode}) => {
         <br/>
         ご迷惑をおかけして申し訳ありません。
       </Content>
+      <Link to="/" style={{display: 'block', textAlign: 'center'}}>
+        <Btn>TOPページへ</Btn>
+      </Link>
     </Wrapper>
   );
 }
